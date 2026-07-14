ca84e69b6bb1a3bcb7cf1046c35250b8e789dcfb
Benjamin Cheng <ben@procss.online>
2026-07-15 02:59:45 +0800

    Update accuracy date to 6 July 2026 in disclaimer and source label

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PAEDSENGAGE_CONFIG = {
   googleMapsApiKey: "REPLACE_WITH_GOOGLE_MAPS_API_KEY",
-  sourceLabel: "Participating PaedsENGAGE Clinics PDF (accurate as of 22 Jun 2026)",
+  sourceLabel: "Participating PaedsENGAGE Clinics PDF (accurate as of 6 July 2026)",
   dataUrl: "./data/clinics.json",
 };
